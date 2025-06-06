ecdcc39267e74d252e8f9abeb74bcf5b6dd3ddcd
Hanae Messari Khali <hanaemessarikhali@gmail.com>
2025-06-06 07:24:23 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,53 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes_Objects
+namespace Access_Modifiers
 {
-    //class is a datatype and if it's a datatype so it's a Data Structure bcs we 
-    //order inside it data 
-    //if we want to use a class for ex : clsEmployee we should have an object
-    //for ex : clsEmployee ITEmployee, SalesEmployee... those are objects type class 
-    //A class is called also BLUE BRIDGE (مخطط)
-    //class is a classification of members and methods 
-    class clsPerson
+    //In c++ there are 3 types of access specifiers/modifiers : private/protected/public
+    //but in c# there are 4 types of access specifiers/modifiers :       + Internal
+
+    class clsA
     {
-        //Property = Field = Member
 
-        private short _Age = 0;  
+        private byte x1 = 10; //for internal use
+        protected byte x2 = 20; //also for internal use & for classes that inherits this class
+        public byte x3 = 30;
 
-       public string FirstName = "";
-       public string LastName = "";
-       public string FullName() //Method = Function
-       {
-           return FirstName + " " + LastName;
-       }
+        private byte Fun1()
+        {
+            return 70;
+        }
 
+        protected byte Fun2()
+        {
+            return 40;
+        }
+
+        public byte Fun3()
+        {
+            return 50;
+        }
 
     }
 
+    class clsB : clsA //clsB inherits clsA
+    {
+        //x2,fun2 is protected and x3,fun3 is public in the base class so we can access to them
+        public byte Fun4() 
+        {
+            return (byte)(x2 + x3 + Fun2() + Fun3()); 
+        }
 
-    internal class Program 
-    {            
+    }
+
+    internal class Program
+    {
         static void Main(string[] args)
         {
-            //create object Person1 type class in memory
-            //assign Person1 in memory
-            clsPerson Person1 = new clsPerson(); 
+            //create object from class
+            clsA A1 = new clsA();
 
-            Person1.FirstName = "Hanae";
-            Person1.LastName = "Messari-Khali";
+            Console.WriteLine("Public members/methods are : ");
+            Console.WriteLine("x3 = {0}", A1.x3);
+            Console.WriteLine("Function 3 = {0}", A1.Fun3());
 
-            Console.WriteLine("My name is : " + Person1.FullName());
-     
+            //we cannot access to x2 and fun2() due to their protection level
+            Console.WriteLine("x2 = {0}, Function 2 = {1}", A1.x2, A1.Fun2());
 
-            //create object Person2 type class clsPerson in memory
-            clsPerson Person2 = new clsPerson();
+            //we cannot access to x1 and fun1() due to their protection level
+            Console.WriteLine("x1 = {0}, Function 1 = {1}", A1.x1, A1.Fun1());
 
-            Person2.FirstName = "Mona";
-            Person2.LastName = "Zaki";
+            clsB B1 = new clsB();
 
-            Console.WriteLine("Her Name is : " + Person2.FullName());
-            Console.ReadLine();
+            Console.WriteLine("Public members/methods are : ");
+            Console.WriteLine("Function 4 = {0}", B1.Fun4());  
+
+            //we cannot access to x2 and fun2() due to their protection level
+            Console.WriteLine("x2 = {0}, Function 2 = {1}", B1.x2, B1.Fun2());
+
+            //we cannot access to x1 and fun1() due to their protection level
+            Console.WriteLine("x1 = {0}, Function 1 = {1}", B1.x1, B1.Fun1());
+
+            Console.ReadKey();
 
 
         }
